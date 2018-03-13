d51119732afc0cfee6655f66a4099656a6757015
elizabethbarney <36878011+elizabethbarney@users.noreply.github.com>
2018-03-13 16:24:17 -0700

    themes, includes

---
+++ includes/config.php
@@ -0,0 +1,98 @@
+<?php
+/*
+config.php
+
+put configuration information here
+
+*/
+
+define('DEBUG',TRUE); #we want to see all errors
+
+define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
+
+$nav1['index.php'] = "HOME";
+$nav1['customers.php'] = "CUSTOMERS";
+$nav1['contact.php'] = "CONTACT";
+$nav1['daily.php'] = "DAILY";
+$nav1['adventures.php'] = "ADVENTURES";
+
+
+//echo THIS_PAGE;
+
+//die;
+
+//defaults for header.php
+$banner = 'EXPLORE MORE';
+$slogan = '';
+
+switch(THIS_PAGE){
+        
+    case'template.php':
+        $title = 'Template Page';
+    break;
+        
+    default:
+        $title = THIS_PAGE;
+}
+
+// other include files referenced here
+include 'credentials.php';
+
+
+
+function myerror($myFile, $myLine, $errorMsg)
+{
+    if(defined('DEBUG') && DEBUG)
+    {
+       echo "Error in file: <b>" . $myFile . "</b> on line: <b>" . $myLine . "</b><br />";
+       echo "Error Message: <b>" . $errorMsg . "</b><br />";
+       die();
+    }else{
+		echo "I'm sorry, we have encountered an error.  Would you like to buy some socks?";
+		die();
+    }
+}
+
+function coffee_links($nav1){
+    
+    foreach ($nav1 as $url => $text){
+        //echo '<li><a href="' . $url . '">' . $text . '</a></li>';
+        
+        if(THIS_PAGE == $url)
+        {//current page - add highlight
+            echo '
+        <li class="nav-item active px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="' . $url . '">' . $text . '</a>
+        </li>
+        ';   
+        }else{//no highlight
+             echo '
+        <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="' . $url . '">' . $text . '</a>
+        </li>
+        ';  
+        }
+        
+     
+    }
+}//end coffee_links()
+
+/*
+
+ <li class="nav-item active px-lg-4">
+    <a class="nav-link text-uppercase text-expanded" href="index.html">Home
+        <span class="sr-only">(current)</span>
+    </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="about.html">About</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="products.html">Products</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="store.html">Store</a>
+            </li>
+
+*/
+
---
+++ includes/credentials.php
@@ -0,0 +1,7 @@
+<?php
+//credentials.php
+
+define('DB_NAME','central_emb_db');
+define('DB_USER','central_emb_ad');
+define('DB_PASSWORD','7203Beth!');
+define('DB_HOST','central.elizabethmbarney.com');
